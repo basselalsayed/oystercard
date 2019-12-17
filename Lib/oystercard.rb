1fdb69844866988f68d28c758081c20db0c568e7
benauld <ben@portobello-consultancy.co.uk>
2019-12-17 14:35:47 +0000

    Chapter 7

--- Lib/oystercard.rb
+++ Lib/oystercard.rb
@@ -1,14 +1,28 @@
 class Oystercard
 
 attr_reader :balance
+MAXIMUM_BALANCE = 90
 
-def initialize(balance = 0)
+def initialize(balance = 0, maximum_balance = MAXIMUM_BALANCE)
   @balance = balance
+  @maximum_balance = maximum_balance
 end
 
 
   def top_up(amount)
+    raise "Top Up above maximum" if balance_exceeded?(amount)
     @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+
+private
+
+  def balance_exceeded?(amount)
+    @maximum_balance < @balance + amount
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,10 +9,18 @@ describe Oystercard do
   #
   #   it { is_expected.to respond_to(:top_up).with(1).argument }
 
-    it 'can top up the balance' do
-      print "top up"
-      p subject.top_up(10)
-      expect(subject.top_up(10)) .to eq(subject.balance)
-    end
-
+  it 'can top up the balance' do
+    expect(subject.top_up(10)) .to eq(subject.balance)
   end
+
+  it "cannot top-up beyond maximum balance" do
+    expect { subject.top_up(Oystercard::MAXIMUM_BALANCE + 1) } .to raise_error "Top Up above maximum"
+  end
+
+  it "can reduce balance by a specified amount" do
+    subject.top_up(11)
+    expect(subject.deduct(10)) .to eq(subject.balance)
+  end
+
+
+end

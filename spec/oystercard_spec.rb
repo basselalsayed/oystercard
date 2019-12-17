46b4dd574fc0d887ca1bc6a7eed9d8058a8c144c
Imraan Ali <imraan91@hotmail.co.uk>
2019-12-17 13:52:04 +0000

    step 2 and 3 defining top up class

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,18 @@
+require 'oystercard'
+
 describe Oystercard do
-end
+  it 'has a balance of zero' do
+    expect(subject.balance).to eq(0)
+  end
+
+  # describe 'top_up' do
+  #
+  #   it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can top up the balance' do
+      print "top up"
+      p subject.top_up(10)
+      expect(subject.top_up(10)) .to eq(subject.balance)
+    end
+
+  end

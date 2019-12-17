c5d3f41c9e8588de4502df6506cb5f648c4ae99c
Imraan Ali <imraan91@hotmail.co.uk>
2019-12-17 15:23:32 +0000

    touch in an out added

--- Lib/oystercard.rb
+++ Lib/oystercard.rb
@@ -2,10 +2,13 @@ class Oystercard
 
 attr_reader :balance
 MAXIMUM_BALANCE = 90
+MINIMUM_TOUCH_IN = 1
+MINIMUM_FARE = 2
 
 def initialize(balance = 0, maximum_balance = MAXIMUM_BALANCE)
   @balance = balance
   @maximum_balance = maximum_balance
+  @in_journey = false
 end
 
 
@@ -14,8 +17,15 @@ end
     @balance += amount
   end
 
-  def deduct(amount)
-    @balance -= amount
+
+  def touch_in
+    raise "insufficient funds" if insufficient_touch_in?
+    @in_journey = true
+  end
+
+  def touch_out
+    deduct(MINIMUM_FARE)
+    @in_journey = false
   end
 
 
@@ -24,5 +34,14 @@ private
   def balance_exceeded?(amount)
     @maximum_balance < @balance + amount
   end
+
+  def insufficient_touch_in?
+    MINIMUM_TOUCH_IN > @balance
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -17,9 +17,23 @@ describe Oystercard do
     expect { subject.top_up(Oystercard::MAXIMUM_BALANCE + 1) } .to raise_error "Top Up above maximum"
   end
 
-  it "can reduce balance by a specified amount" do
+  it "can detect when oystercard has touched in" do
+    subject.top_up(5)
+    expect(subject.touch_in) .to eq true
+  end
+
+  it "can detect when you have touched out" do
+    expect(subject.touch_out) .to eq false
+  end
+
+  it "fails to allow touch in if insufficient funds" do
+    expect { subject.touch_in } .to raise_error "insufficient funds"
+  end
+
+  it "touch out reduced balance by minimum fare" do
     subject.top_up(11)
-    expect(subject.deduct(10)) .to eq(subject.balance)
+    subject.touch_in
+    expect { subject.touch_out} .to change {subject.balance } .by(-Oystercard::MINIMUM_FARE)
   end
 
 
